c79d0eb6e01b90a6d7cdd45f625d7b0bbb230de9
EC2 Default User <ec2-user@ip-172-31-1-53.ec2.internal>
2022-05-28 15:56:22 +0000

    Implement csv upload

--- sleepyhead_csv/app.py
+++ sleepyhead_csv/app.py
@@ -1,7 +1,51 @@
 import json
+import csv
+import boto3
+
+s3 = boto3.client('s3')
+ddb = boto3.client('dynamodb')
+
+ddb_S_fields = [
+    'DayId',
+    'PhoneDownTime',
+    'Activities',
+    'Bedtime',
+    'LightsOutTime',
+    'HowLongToSleep',
+    'WakeUpCount',
+    'WakeUpDuration',
+    'FinalWakeUpTime',
+    'AriseTime',
+    'SleepNotes',
+    'FitbitHours',
+    'FitbitMins',
+    'FitbitScore',
+    'ExerciseTime',
+    'Exercises',
+    'Stress',
+    'Mood',
+    'MorningEnergy',
+    'ForenoonEnergy',
+    'AfternoonEnergy',
+    'EveningEnergy'
+    ]
+    
+ddb_N_fields = [
+    'SleepDurationInMinutes',
+    'IdleWakeupDurationInMinutes',
+    'AverageEnergyScore'
+    ]
 
 def lambda_handler(event, context):
-    print(event)
+    sd_rows = construct_objects()
+    sd_headers = ddb_S_fields + ddb_N_fields
+    
+    with open('sleep_data.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = sd_headers)
+        writer.writerows(sd_rows)
+        
+    s3.upload_file('sleep_data.csv', 'sleep-data-swarnatonse', 'data/sleep_data.csv')
+    
 
     return {
         "statusCode": 200,
@@ -11,3 +55,27 @@ def lambda_handler(event, context):
             }
         ),
     }
+    
+def construct_objects():
+    items = scan_ddb()
+    sd_rows = []
+    
+    for item in items:
+        sd_row = dict()
+        for ddb_s_field in ddb_S_fields:
+            if item.get(ddb_s_field):
+                sd_row[ddb_s_field] = item.get(ddb_s_field).get('S')
+        for ddb_n_field in ddb_N_fields:
+            if item.get(ddb_n_field):
+                sd_row[ddb_n_field] = item.get(ddb_n_field).get('N')
+        sd_rows.append(sd_row)
+                
+    print(json.dumps(sd_rows))
+    return sd_rows
+
+        
+def scan_ddb():
+    ddb_response = ddb.scan(
+            TableName='SleepData'
+        )
+    return ddb_response.get('Items')
