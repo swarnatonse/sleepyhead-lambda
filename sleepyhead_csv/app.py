2522df22e8303e5453dcd26533f224303cb30625
EC2 Default User <ec2-user@ip-172-31-1-53.ec2.internal>
2022-05-29 00:43:47 +0000

    Add sleep report generation

--- sleepyhead_csv/app.py
+++ sleepyhead_csv/app.py
@@ -1,6 +1,8 @@
 import json
 import csv
 import boto3
+from operator import itemgetter
+from datetime import datetime
 
 s3 = boto3.client('s3')
 ddb = boto3.client('dynamodb')
@@ -35,17 +37,23 @@ ddb_N_fields = [
     'IdleWakeupDurationInMinutes',
     'AverageEnergyScore'
     ]
+    
+sleep_report_fields = [
+    'DayId',
+    'Bedtime',
+    'LightsOutTime',
+    'HowLongToSleep',
+    'WakeUpCount',
+    'WakeUpDuration',
+    'FinalWakeUpTime',
+    'AriseTime',
+    'SleepNotes',  
+    ]
 
 def lambda_handler(event, context):
-    sd_rows = construct_objects()
-    sd_headers = ddb_S_fields + ddb_N_fields
     
-    with open('/tmp/sleep_data.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = sd_headers)
-        writer.writerows(sd_rows)
-        
-    s3.upload_file('/tmp/sleep_data.csv', 'sleep-data-swarnatonse', 'data/sleep_data.csv')
-    
+    upload_sleep_data()
+    upload_sleep_report()
 
     return {
         "statusCode": 200,
@@ -55,6 +63,16 @@ def lambda_handler(event, context):
             }
         ),
     }
+    
+def upload_sleep_data():
+    sd_rows = construct_objects()
+    sd_headers = ddb_S_fields + ddb_N_fields
+    
+    with open('/tmp/sleep_data.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = sd_headers)
+        writer.writerows(sd_rows)
+        
+    s3.upload_file('/tmp/sleep_data.csv', 'sleep-data-swarnatonse', 'data/sleep_data.csv')
     
 def construct_objects():
     items = scan_ddb()
@@ -69,10 +87,33 @@ def construct_objects():
             if item.get(ddb_n_field):
                 sd_row[ddb_n_field] = item.get(ddb_n_field).get('N')
         sd_rows.append(sd_row)
-                
-    print(json.dumps(sd_rows))
-    return sd_rows
 
+    return sd_rows
+    
+def upload_sleep_report():
+    sd_objs = construct_objects()
+    sorted_sd_objs = sorted(sd_objs, key=itemgetter('DayId')) 
+    sr_rows = []
+    
+    for field in sleep_report_fields:
+        row = [field]
+        for sd_obj in sorted_sd_objs:
+            if sd_obj.get(field):
+                if field == "DayId":
+                    dateobj = datetime.strptime(sd_obj.get(field), "%Y-%m-%d")
+                    row.append(dateobj.strftime("%A | %d-%m-%Y"))
+                else:
+                    row.append(sd_obj.get(field))
+            else:
+                row.append("-")
+        sr_rows.append(row)
+        
+    print(json.dumps(sr_rows))
+        
+    with open('sleep_report.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(sr_rows)
+    s3.upload_file('sleep_report.csv', 'sleep-data-swarnatonse', 'reports/sleep_report.csv')
         
 def scan_ddb():
     ddb_response = ddb.scan(
